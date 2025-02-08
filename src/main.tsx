2c9d201dcdeea0d472d49f15aee7e66dfde8bc4b
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-02-08 16:22:17 +0900

    chore: msw 설치 및 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
 import { BrowserRouter } from "react-router-dom";
+import { MSWComponent } from './components/MSWComponent.tsx';
 
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+  <MSWComponent/>
     <App />
   </BrowserRouter>
 );
