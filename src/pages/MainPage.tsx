dc1205f3fbd9e818c63b74675e3f5bc087a736f4
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-14 03:52:35 +0900

    feat: MainPage 구현
    채팅 입력창, 채팅 가이드바, 사용자 메시지 창, ai 응답 메시지 창을 추가한 MainPage 컴포넌트

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,57 @@
+import styles from "../styles/main/MainPage.module.less";
+import Header from "../components/Header";
+import GuideBar from "../components/main/GuideBar";
+import { CHAT_GUIDE } from "../data/chatGuide";
+import ChatInput from "../components/main/ChatInput";
+import { useState } from "react";
+import UserMessage from "../components/main/UserMessage";
+import AIMessage from "../components/main/AIMessage";
+import { CHAT_RESPONSES } from "../data/chatResponses";
+
+const MainPage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState<
+    { text: string; sender: "user" | "lumi" }[]
+  >([]);
+
+  const handleSendMessage = () => {
+    if (CHAT_RESPONSES[inputValue]) {
+      setMessages([
+        ...messages,
+        { text: inputValue, sender: "user" },
+        { text: CHAT_RESPONSES[inputValue], sender: "lumi" },
+      ]);
+      setInputValue("");
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}>1</div>
+      <div className={styles.mainContainer}>
+        <div className={styles.header}>
+          <Header />
+        </div>
+        <div className={styles.chatContainer}>
+          <div className={styles.messageContainer}>
+            {messages.map((message, index) => {
+              if (message.sender === "user") {
+                return <UserMessage message={message.text} key={index} />;
+              } else {
+                return <AIMessage message={message.text} key={index} />;
+              }
+            })}
+          </div>
+          <div className={styles.bottomContainer}>
+            <GuideBar guideBar={CHAT_GUIDE} setInputValue={setInputValue} />
+            <ChatInput
+              handleSendMessage={handleSendMessage}
+              inputValue={inputValue}
+              setInputValue={setInputValue}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MainPage;
