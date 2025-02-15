87f1fc9cd49578f61d80653e2d53f3ac25d958db
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-15 22:11:20 +0900

    fix: CustomAxios import 경로 수정

--- src/service/getChatting.ts
+++ src/service/getChatting.ts
@@ -1,23 +1,24 @@
 import { QueryFunction } from "@tanstack/react-query";
-import CustomAxios from "../../api/CustomAxios";
+import CustomAxios from "../api/CustomAxios";
 
-export const getChatting: QueryFunction<Chat[], [_1: string, number]>
- = async ({ queryKey: [, chatRoomId] }) => {
+export const getChatting: QueryFunction<Chat[], [_1: string, number]> = async ({
+  queryKey: [, chatRoomId],
+}) => {
   try {
-    if (!chatRoomId) { // id가 없다면 예외 처리
+    if (!chatRoomId) {
+      // id가 없다면 예외 처리
       throw new Error("chatRoomId is required");
-    };
+    }
 
     const res = await CustomAxios.get(`/api/chatting/${chatRoomId}`, {
       headers: {
-        'Cache-Control': 'no-store',
+        "Cache-Control": "no-store",
       },
     });
 
     return res.data.result.chatting;
-
-  } catch(err) {
+  } catch (err) {
     console.error("Failed to fetch data", err);
     throw err;
   }
- }
+};
