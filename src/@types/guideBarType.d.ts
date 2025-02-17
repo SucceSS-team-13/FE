b1421a6feefad66f8537da730cf915dd571521c7
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-17 13:25:51 +0900

    feat: 채팅방 데이터 타입 추가

--- src/@types/guideBarType.d.ts
+++
@@ -1,3 +0,0 @@
-interface GuideBar {
-  title: string;
-}
--- src/@types/mainPageType.d.ts
+++ src/@types/mainPageType.d.ts
@@ -1,6 +1,18 @@
 interface Chat {
+  // 채팅 데이터 타입
   id: number;
   sender?: "user" | "lumi";
   text: string;
   location?: string;
+}
+interface GuideBar {
+  // 가이드 바 데이터 타입
+  title: string;
+}
+
+interface ChatRoom {
+  // 채팅방 데이터 타입
+  id: string;
+  title: string;
+  lastMessageDate: Date;
 }
