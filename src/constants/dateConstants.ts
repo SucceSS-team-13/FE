f661e9d0057d9285380c81842e0eb2552a3af669
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-17 13:25:22 +0900

    feat: 날짜 관련 상수 데이터 분리

---
+++ src/constants/dateConstants.ts
@@ -0,0 +1,9 @@
+export const DATE_GROUP = {
+  TODAY: "오늘",
+  YESTERDAY: "어제",
+  LAST_WEEK: "지난 7일",
+  LAST_MONTH: "지난 30일",
+  OLDER: "이전 메시지",
+} as const;
+
+export const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
