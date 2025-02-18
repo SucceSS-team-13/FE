0044f571d6217e06295945966f21a696c6a0fcc3
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-19 02:38:10 +0900

    feat: 인피니트 스크롤링 커스텀 훅 추가

---
+++ src/hook/useInfiniteScroll.ts
@@ -0,0 +1,35 @@
+import { useInfiniteQuery, QueryFunction } from "@tanstack/react-query";
+import { useRef, useCallback } from "react";
+import { InfiniteData } from "@tanstack/react-query";
+
+export const useInfiniteScroll = <T>({
+  queryKey,
+  queryFn,
+  getNextPageParam,
+}: UseInfiniteScrollProps<T>) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<T, Error, InfiniteData<T>, [string], number>({
+      queryKey: [queryKey],
+      queryFn: queryFn as QueryFunction<T, [string], number>,
+      getNextPageParam,
+      initialPageParam: 1,
+    });
+
+  const observer = useRef<IntersectionObserver | null>(null); // 마지막 요소인지 확인하기 위한 ref
+  const lastElementRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (isFetchingNextPage) return; // 데이터를 가져오는 중이라면 return
+      if (observer.current) observer.current.disconnect(); // 이미 존재하는 observer가 있다면 제거
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          // 마지막 요소가 보이고 데이터가 더 있다면
+          fetchNextPage(); // 다음 페이지 데이터 가져오기
+        }
+      });
+      if (node) observer.current.observe(node); // 마지막 요소가 보이면 observer 추가
+    },
+    [isFetchingNextPage, fetchNextPage, hasNextPage] // 의존성 배열
+  );
+
+  return { data, lastElementRef, isFetchingNextPage }; // 데이터, 마지막 요소 ref, 데이터를 가져오는 중인지 여부 반환
+};
