66d559f7d6ab91f9930ac5642ce680afd0326017
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-19 03:45:26 +0900

    style: 코드 포맷팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import "./index.css"
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { MSWComponent } from './components/MSWComponent.tsx';
-import RQProvider from './components/RQProvider.tsx';
+import { MSWComponent } from "./components/MSWComponent.tsx";
+import RQProvider from "./components/RQProvider.tsx";
 
 const loadKakaoMaps = () => {
   return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_API_KEY}&libraries=services&autoload=false`;
-    
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
+      import.meta.env.VITE_KAKAO_API_KEY
+    }&libraries=services&autoload=false`;
+
     script.onload = () => {
       // SDK가 로드된 후 maps를 초기화
       window.kakao.maps.load(() => {
         resolve(null);
       });
     };
-    
+
     script.onerror = (error) => {
       console.error("카카오 지도 로딩 중 오류 발생생:", error); // 디버깅용
       reject(error);
@@ -29,7 +30,7 @@ const loadKakaoMaps = () => {
 const renderApp = () => {
   createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
-      <MSWComponent/>
+      <MSWComponent />
       <RQProvider>
         <App />
       </RQProvider>
