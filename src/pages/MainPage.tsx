0fe242fab391ff0af50c8750529ad621098b2b83
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2025-02-19 20:46:58 +0900

    fix: setInputValue("") 위치 수정
    사용자가 메시지를 전송하고 적었던 텍스트가 바로 지워지게 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -43,8 +43,10 @@ const MainPage = () => {
 
   const postChat = useMutation({
     mutationFn: async () => {
+      const messageText = inputValue;
+      setInputValue("");
       return CustomAxios.post(`/api/chatting/${1}`, {
-        text: inputValue,
+        text: messageText,
       });
     },
     onMutate() {
@@ -71,7 +73,6 @@ const MainPage = () => {
       });
     },
     onSuccess: (response) => {
-      setInputValue("");
       const recomment = response.data.result;
       const lumiResponse: Chat = {
         id: recomment.id,
