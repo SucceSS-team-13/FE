4e4eefc957f83a3a4ac9d7fd3b0a1f645a2ad058
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-23 01:59:31 +0900

    style: 코드 포맷팅

--- src/components/main/MessageContainer.tsx
+++ src/components/main/MessageContainer.tsx
@@ -28,18 +28,22 @@ const MessageContainer = ({
       <div className={styles.messagesWrapper}>
         {hasNextPage && !isFetchingNextChat && !isThrottled && (
           <div ref={lastElementRef} className={styles.loadingTrigger}>
-            <LoadingSpinner size={'sm'} />
+            <LoadingSpinner size={"sm"} />
           </div>
         )}
         {reversedMessages.map((message, index) => {
           if (message.sender === "user") {
-            return <UserMessage message={message.text} key={message.id || index} />;
+            return (
+              <UserMessage message={message.text} key={message.id || index} />
+            );
           } else {
             return (
               <AIMessage
                 message={message.text}
-                key={message.id || index}
-                isLoading={message.sender === "lumi" && message.text === "" && isPending}
+                key={message.id}
+                isLoading={
+                  message.sender === "lumi" && message.text === "" && isPending
+                }
                 location={message.location}
               />
             );
