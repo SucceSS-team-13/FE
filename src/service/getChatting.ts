59a3c233dd742fe4e24a61455c0019f6fd35230a
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-23 02:01:37 +0900

    feat:채팅 검색 api 호출 함수 정의

--- src/service/getChatting.ts
+++ src/service/getChatting.ts
@@ -1,22 +1,27 @@
 import { QueryFunction } from "@tanstack/react-query";
 import CustomAxios from "../api/CustomAxios";
 
-export const getChatting: QueryFunction<Chat[], [_1: string, number], number> = async ({
-  queryKey, pageParam
-}) => {
+export const getChatting: QueryFunction<
+  Chat[],
+  [_1: string, number],
+  number
+> = async ({ queryKey, pageParam }) => {
   try {
     const chatRoomId = queryKey[1];
-    
+
     if (chatRoomId === undefined || chatRoomId === null) {
       // id가 없다면 예외 처리
       throw new Error("chatRoomId is required");
     }
 
-    const res = await CustomAxios.get(`/api/chat/room/${chatRoomId}?page=${pageParam}&size=1`, {
-      headers: {
-        "Cache-Control": "no-store",
-      },
-    });
+    const res = await CustomAxios.get(
+      `/api/chat/room/${chatRoomId}?page=${pageParam}&size=1`,
+      {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      }
+    );
 
     return res.data.result.content;
   } catch (err) {
@@ -35,6 +40,25 @@ export const getChatRoomList = async ({
       `/api/chatRoomList?page=${pageParam}`
     );
     console.log("채팅방 목록 ", response.data.result);
+    return response.data;
+  } catch (err) {
+    console.error("Failed to fetch data", err);
+    throw err;
+  }
+};
+
+export const getSearchChatRoomList = async ({
+  pageParam,
+  searchText,
+}: {
+  pageParam: number | undefined;
+  searchText: string;
+}) => {
+  try {
+    const response = await CustomAxios.get(
+      `/api/searchChatRoomList?page=${pageParam}&search=${searchText}`
+    );
+    console.log("검색 api 호출", response.data.result);
     return response.data;
   } catch (err) {
     console.error("Failed to fetch data", err);
