85980d9a8414399833a0d78125b63979c48dd82d
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-23 01:58:26 +0900

    feat: 채팅 검색 모달 구현
    - 디바운스를 적용하여 사용자 입력 0.3초 후 검색 api 호출
    - modalRef를 생성해 모달창 밖을 클릭 할 경우 모달창이 닫히도록 구현
    - 검색결과 인피니트 스크롤 적용

---
+++ src/components/main/SearchModal.tsx
@@ -0,0 +1,138 @@
+import styles from "../../styles/main/SearchModal.module.less";
+import ActionIcon from "./ActionIcon";
+import { useState, useEffect, useRef } from "react";
+import { useInfiniteScroll } from "../../hook/useInfiniteScroll";
+import { getSearchChatRoomList } from "../../service/getChatting";
+import { groupChatsByDate } from "../../utils/dateUtils";
+import Loading from "../../components/Loading";
+
+const SearchModal = ({
+  setSearchModal,
+  searchModal,
+}: {
+  setSearchModal: (status: boolean) => void;
+  searchModal: boolean;
+}) => {
+  const [searchText, setSearchText] = useState<string>("");
+  const [debouncedSearchText, setDebouncedSearchText] = useState<string>("");
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // 디바운스 로직
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearchText(searchText);
+    }, 300);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchText]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // 모달창 외부 클릭시 모달창을 닫는 함수
+      if (
+        modalRef.current && // modalRef.current: useRef를 사용해 모달창의 DOM 요소를 참조, modalRef.current는 모달창의 실제 DOM 노드
+        !modalRef.current.contains(event.target as Node) // modalRef.current가 존재하고, 클릭된 요소가 modalRef.current의 자식 요소가 아니라면(모달창 내부가 아니라면)
+      ) {
+        setSearchModal(false);
+      }
+    };
+
+    // 이벤트 리스너 추가
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // 클린업 함수로 이벤트 리스너 제거
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [setSearchModal]);
+
+  const {
+    data: searchChatRoomData,
+    lastElementRef: searchChatRoomLastElementRef,
+    isFetchingNextPage: isFetchingNextSearchChatRoom,
+  } = useInfiniteScroll<ChatRoomResponse, [string, string]>({
+    queryKey: ["searchChatRoomList", debouncedSearchText],
+    queryFn: async ({ pageParam }) => {
+      const response = await getSearchChatRoomList({
+        pageParam,
+        searchText: debouncedSearchText, // 디바운스된 검색어 사용
+      });
+      return response;
+    },
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+  const searchChatRoomList =
+    searchChatRoomData?.pages.flatMap((page) => page.result || []) || [];
+  const groupedChats = groupChatsByDate(searchChatRoomList);
+  return (
+    <div className={styles.container}>
+      <div ref={modalRef} className={styles.searchModal}>
+        <div className={styles.searchModalHeader}>
+          <input
+            type="text"
+            placeholder="채팅 검색..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <ActionIcon
+            icon="/image/close.png"
+            size="small"
+            onClick={() => setSearchModal(!searchModal)}
+          />
+        </div>
+        <div
+          className={`${styles.newChatContainer} ${
+            debouncedSearchText ? styles.searchTextTrue : styles.searchTextFalse
+          }`}
+        >
+          {!debouncedSearchText && (
+            <div className={styles.newChatButton}>
+              <button>
+                <img src="/image/newChat.png" alt="newChat" />
+                <span>새 채팅</span>
+              </button>
+            </div>
+          )}
+        </div>
+        <div className={styles.chatListContainer}>
+          {!searchChatRoomList?.length ? (
+            <Loading text="검색 결과가 없어요!" size="medium" />
+          ) : (
+            <ul>
+              {Object.entries(groupedChats).map(([date, chats]) => (
+                <div key={date} className={styles.chatRoomListGroup}>
+                  <span className={styles.chatRoomDate}>{date}</span>
+                  {chats.map((chat) => (
+                    <li key={chat.id} className={styles.chatRoom}>
+                      <img src="/image/searchChat.png" alt="chat" />
+                      <a
+                        href={`/main/${chat.id}`}
+                        className={styles.chatRoomLink}
+                      >
+                        {chat.title}
+                      </a>
+                    </li>
+                  ))}
+                  <div
+                    ref={searchChatRoomLastElementRef}
+                    className={styles.endRef}
+                  ></div>
+                </div>
+              ))}
+              {isFetchingNextSearchChatRoom && (
+                <div className={styles.loading}>
+                  <Loading
+                    text="루미가 검색 결과를 가져오는 중이에요!"
+                    size="small"
+                  />
+                </div>
+              )}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchModal;
