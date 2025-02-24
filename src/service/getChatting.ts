6e7654c0d19078c5601a929ed75b2c0287f29fec
leemanjae02 <144561017+leemanjae02@users.noreply.github.com>
2025-02-24 18:58:20 +0900

    fix: 데이터 타입 수정에 의한 코드 변경

--- src/service/getChatting.ts
+++ src/service/getChatting.ts
@@ -40,7 +40,8 @@ export const getChatRoomList = async ({
       // `/api/chatRoomList?page=${pageParam}`
       `/api/chat/rooms?page=${pageParam}&size=10`
     );
-    console.log("채팅방 목록 가져오기", response.data);
+
+    console.log("채팅방 목록 가져오기", response.data.result.content);
     return response.data;
   } catch (err) {
     console.error("Failed to fetch data", err);
